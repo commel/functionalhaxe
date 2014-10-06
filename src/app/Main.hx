da69e0a094daaf9503d9fa8876cb61150e3ee480
commel <commel@gmail.com>
2014-10-06 14:49:06 +0000

    Refactored Switch to Enum - thanks to @ousado

--- src/app/Main.hx
+++ src/app/Main.hx
@@ -1,5 +1,8 @@
 package app;
 
+
+
+
 class Main {
 
   static function printer(text: String, f: (Int -> Int -> Int), a:Int, b:Int) {
@@ -10,18 +13,18 @@ class Main {
     printer("addition", function(a:Int, b:Int) return a + b, 5, 6);
     printer("multiply", function(a:Int, b:Int) return a * b, 5, 6);
     
-    FuncMatching.down(5);
+    countdown(Value(5));
   } 
+  
+  static function countdown(a: Countdown) {
+    switch (a) {
+        case Value(0): trace("Done");
+        case Value(n): trace(n); countdown(Value(n-1));
+    }
+  }
 
 }
 
-class FuncMatching {
-  
-  public static function down(i) {
-    switch (i) {
-        case 0: trace("Done");
-        case n: trace(n); down(n-1);
-    }
-  }
-  
+enum Countdown {
+  Value(i:Int);
 }
