50f4e3a14191248d1045e3eac25aee1cfb1874c2
commel <commel@gmail.com>
2014-10-06 14:08:40 +0000

    Restructured. Added pattern matching function stuff

--- src/app/Main.hx
+++ src/app/Main.hx
@@ -1,3 +1,5 @@
+package app;
+
 class Main {
 
   static function printer(text: String, f: (Int -> Int -> Int), a:Int, b:Int) {
@@ -7,8 +9,19 @@ class Main {
   static function main():Void {
     printer("addition", function(a:Int, b:Int) return a + b, 5, 6);
     printer("multiply", function(a:Int, b:Int) return a * b, 5, 6);
+    
+    FuncMatching.down(5);
   } 
 
+}
 
-
+class FuncMatching {
+  
+  public static function down(i) {
+    switch (i) {
+        case 0: trace("Done");
+        case n: trace(n); down(n-1);
+    }
+  }
+  
 }
